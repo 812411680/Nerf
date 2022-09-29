b6ded20f9ef794999c05f64c57250c991da7487b
Zhang Franklin <franklin.zhang@qq.com>
2022-09-29 10:41:57 +0800

    500medel-added

--- run_nerf.py
+++ run_nerf.py
@@ -562,11 +562,11 @@ def config_parser():
                         help='frequency of console printout and metric loggin')
     parser.add_argument("--i_img",     type=int, default=500,
                         help='frequency of tensorboard image logging')
-    parser.add_argument("--i_weights", type=int, default=10000,
+    parser.add_argument("--i_weights", type=int, default=500,
                         help='frequency of weight ckpt saving')
-    parser.add_argument("--i_testset", type=int, default=50000,
+    parser.add_argument("--i_testset", type=int, default=500,
                         help='frequency of testset saving')
-    parser.add_argument("--i_video",   type=int, default=50000,
+    parser.add_argument("--i_video",   type=int, default=500,
                         help='frequency of render_poses video saving')
 
     return parser
@@ -743,7 +743,8 @@ def train():
         print('done')
         i_batch = 0
 
-    N_iters = 1000000
+    #N_iters = 1000000
+    N_iters = 10000
     print('Begin')
     print('TRAIN views are', i_train)
     print('TEST views are', i_test)
